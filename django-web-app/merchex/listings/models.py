66df8b76e16ea313748f139e271f2b66bc51260b
Bastien Schwitz <bschwitz33@gmail.com>
2023-11-12 22:41:30 +0100

    update

--- django-web-app/merchex/listings/models.py
+++ django-web-app/merchex/listings/models.py
@@ -1,10 +1,32 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Band(models.Model):
+
+	class Genre(models.TextChoices):
+		HIP_HOP = 'HH'
+		SYNTH_POP = 'SP'
+		ALTERNATIVE_ROCK = 'AR'
+
 	name = models.fields.CharField(max_length=100)
-	name2 = models.fields.CharField(max_length=100)
+	genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+	biography = models.fields.CharField(max_length=1000)
+	year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2023)])
+	active = models.fields.BooleanField(default=True)
+	official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Listing(models.Model):
+
+	class Types(models.TextChoices):
+		RECORDS = 'RE'
+		CLOTHING = 'CL'
+		POSTERS = 'PO'
+		MISCELLANEOUS = 'MI'
+	
 	title = models.fields.CharField(max_length=100)
+	description = models.fields.CharField(max_length=1000)
+	sold = models.fields.BooleanField(default=False)
+	year = models.fields.IntegerField(validators=[MinValueValidator(1800), MaxValueValidator(2023)], null=True)
+	type = models.fields.CharField(choices=Types.choices, max_length=5)
